0a69c6212a177c73babf443277b3502f038180ab
SistemaPayEmpleos <corporativo.hm.next2@gmail.com>
2025-02-19 16:50:32 -0600

    19/02/2025
    se agrego el git ignore para no cargar archivos de python , visual code o de virtual env

--- src/static/js/rdf.js
+++ src/static/js/rdf.js
@@ -1,15 +1,15 @@
-pay = [{opccion:"usuarios"},{opccion:"vacantes"},{opccion:"candidatos"},{opccion:"pruebaspscometricas"}];
-basesirec = [{opccion:"usuarios"},{opccion:"clientes"},{opccion:"reportes"}];
-sistema_info = [];
+pay = [{opccion:"usuarios"},{opccion:"vacantes"},{opccion:"candidatos"},{opccion:"pruebaspscometricas"}];// opciones de las colecciones de payempleos
+basesirec = [{opccion:"usuarios"},{opccion:"clientes"},{opccion:"reportes"}]; // opciones de coleciones de base-sirec
+sistema_info = [];// opciones de las colecciones de sistemas de informacion.
 
-selectSistema = document.getElementById('sistema');
-selectColeccion = document.getElementById('respaldo');
+selectSistema = document.getElementById('sistema'); // btencion del select sistema
+selectColeccion = document.getElementById('respaldo');// obtencion del select de respaldo esto para seleccionar la coleccion que se desea hacer respaldo
 
-selectSistema.addEventListener('change',()=>{
+selectSistema.addEventListener('change',()=>{ // evento para cambiar las opciones de selectColeccion
     selectColeccion.innerHTML = "";
     switch(selectSistema.value){
         case 'payempleos':
-            pay.forEach(opcion => {
+            pay.forEach(opcion => {   // lee los datos de objetos de las colecciones de pay para establecerlas en en el select
                 const option = document.createElement('option');
                 option.value = opcion.opccion;
                 option.textContent = opcion.opccion;
@@ -17,7 +17,7 @@ selectSistema.addEventListener('change',()=>{
             });
             break;
         case 'base-sirec':
-            basesirec.forEach(opcion => {
+            basesirec.forEach(opcion => {  // lee los datos de objetos de las colecciones de base sirec para establecerlas en en el select
                 const option = document.createElement('option');
                 option.value = opcion.opccion;
                 option.textContent = opcion.opccion;
@@ -25,7 +25,7 @@ selectSistema.addEventListener('change',()=>{
             });
             break;
         case 'sistemas-de-informacion':
-            sistema_info.forEach(opcion => {
+            sistema_info.forEach(opcion => { // lee los datos de objetos de las colecciones de sistemas de informacion para establecerlas en en el select
                 const option = document.createElement('option');
                 option.value = opcion.opccion;
                 option.textContent = opcion.opccion;
